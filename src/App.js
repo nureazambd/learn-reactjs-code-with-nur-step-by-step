f4248bb00b6ba71cce5e3cbff4968399a354e533
nureazambd <nur.azam.bd@gmail.com>
2023-10-26 16:41:00 +0600

    componentDidMount life cycle method

--- src/App.js
+++ src/App.js
@@ -3,13 +3,10 @@ import "./App.css";
 import User from './components/User';
 
 export default function App() {
-  const [name,setName] = useState("Nur");
-  console.log(name)
   return (
     <div className="App">
       <h1>App</h1>
-      <User name={name}/>
-      <button onClick={()=>setName("nur-e-azam")}>Update name</button>
+      <User/>
     </div>
   )
 }
--- src/components/User.js
+++ src/components/User.js
@@ -1,11 +1,25 @@
 import React, { Component } from 'react'
 
 export default class User extends Component {
-  render() {
-    console.log("user render method called",this.props)
+    constructor()
+    {
+        super();
+        this.state={
+            name:"Salman"
+        }
+        console.warn("constructor call")
+    }
+    componentDidMount()
+    {
+        console.warn("componentDidMount call")
+    }
+
+    render() {
+        console.warn("render call")
     return (
       <div>
-        <h1>User Component {this.props.name}</h1>
+        <h1>User Component {this.state.name}</h1>
+        <button onClick={()=>{this.setState({name:"Shuvro"})}}>Update Name</button>
       </div>
     )
   }
