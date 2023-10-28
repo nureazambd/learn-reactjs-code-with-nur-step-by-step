165f4b45baac810c61956f38cdaaec68e30343d1
nureazambd <nur.azam.bd@gmail.com>
2023-10-28 14:10:32 +0600

    componentWillUnmount life cycle method

--- src/App.js
+++ src/App.js
@@ -3,10 +3,15 @@ import "./App.css";
 import User from './components/User';
 
 export default function App() {
+  const [show,setShow] = useState(true);
   return (
     <div className="App">
       <h1>App</h1>
-      <User/>
+      <div>
+      {show? <User/>:<h1>component remove!!</h1>}
+      </div>
+      
+      <button onClick={()=>{setShow(!show)}}>Click to show/hide</button>
     </div>
   )
 }
--- src/components/User.js
+++ src/components/User.js
@@ -1,39 +1,43 @@
 import React, { Component } from 'react'
 
 export default class User extends Component {
-    constructor()
-    {
-        super();
-        this.state={
-            name:"Salman",
-            count:0
-        }
-        console.warn("constructor call")
-    }
-    componentDidMount()
-    {
-        console.warn("componentDidMount call")
-    }
-    componentDidUpdate(preProps, preState, snapshot){
-        console.warn("componentDidUpdate call",preState.count,this.state.count)
-        if(this.state.count === 3){
-            alert("State count is 3")
-        }
-    }
-    shouldComponentUpdate(){
-        console.warn("shouldComponentUpdate", this.state.count)
-        if(this.state.count>=2 && this.state.count<5){
-            return true
-        }
+    // constructor()
+    // {
+    //     super();
+    //     this.state={
+    //         name:"Salman",
+    //         count:0
+    //     }
+    //     console.warn("constructor call")
+    // }
+    // componentDidMount()
+    // {
+    //     console.warn("componentDidMount call")
+    // }
+    // componentDidUpdate(preProps, preState, snapshot){
+    //     console.warn("componentDidUpdate call",preState.count,this.state.count)
+    //     if(this.state.count === 3){
+    //         alert("State count is 3")
+    //     }
+    // }
+    // shouldComponentUpdate(){
+    //     console.warn("shouldComponentUpdate", this.state.count)
+    //     if(this.state.count>=2 && this.state.count<5){
+    //         return true
+    //     }
         
+    // }
+    componentWillUnmount(){
+        console.warn("componentWillUnmount called")
     }
 
     render() {
         console.warn("render call")
     return (
       <div>
-        <h1>User Component {this.state.count}</h1>
-        <button onClick={()=>{this.setState({count:this.state.count+1})}}>Update Name</button>
+        <h1>User component</h1>
+        {/* <h1>User Component {this.state.count}</h1> */}
+        {/* <button onClick={()=>{this.setState({count:this.state.count+1})}}>Update Name</button> */}
       </div>
     )
   }
