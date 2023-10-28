cf8d4124ccde90aedcdee4cc6af35329b55f51af
nureazambd <nur.azam.bd@gmail.com>
2023-10-28 17:10:12 +0600

    udeEffect with condition

--- src/App.js
+++ src/App.js
@@ -5,15 +5,19 @@ import Student from './components/Student';
 
 export default function App() {
   const [show,setShow] = useState(true);
+  const [count,setCount] = useState(100);
+
   return (
     <div className="App">
       <h1>App</h1>
-      <Student/>
+      <Student count={count}/>
       <div>
       {show? <User/>:<h1>component remove!!</h1>}
       </div>
       
-      <button onClick={()=>{setShow(!show)}}>Click to show/hide</button>
+      {/* <button onClick={()=>{setShow(!show)}}>Click to show/hide</button> */}
+      <button onClick={()=>{setCount(count+1)}}>Student count</button>
+
     </div>
   )
 }
--- src/components/Student.js
+++ src/components/Student.js
@@ -1,15 +1,15 @@
 import React,{useEffect,useState} from 'react'
 
-export default function Student() {
+export default function Student(props) {
     const [name,setName] = useState(" ");
     const [count,setCount] = useState(0);
     useEffect(()=>{
-        console.log("useEffect call")
-    },[count])
+        console.log("useEffect call",props.count)
+    },[props.count])
   return (
     <div>
-        <h1>Student {count}</h1>
-        <button onClick={()=>{setCount(count+1)}}>Click</button>
+        <h1>Student {props.count}</h1>
+        <button onClick={()=>{}}>Click</button>
     </div>
   )
 }
