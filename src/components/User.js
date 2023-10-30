506a4d0ba940614cb267c98c33f50994492b22de
nureazambd <nur.azam.bd@gmail.com>
2023-10-30 14:50:22 +0600

    functional component ref used

--- src/components/User.js
+++ src/components/User.js
@@ -1,41 +1,31 @@
-import React, { Component, createRef } from 'react'
+import React,{useState, useRef} from 'react'
 
-class User extends Component {
-  constructor(){
-    super();
-    this.inputRefName = createRef();
-    this.inputRefEmail = createRef();
-    this.inputRefPass = createRef();
-    this.buttonRef = createRef();
-  }
-  componentDidMount(){
-    console.log(this.inputRefName.current.value='Salman')
-  }
-  getRef(){
-    console.log(this.inputRefName.current.value='Amir')
-    this.inputRefName.current.style.color='white'
-    this.inputRefName.current.style.background='black'
-    console.log(this.inputRefEmail.current.value='amir@gmail.com')
-    this.inputRefEmail.current.style.color='black'
-    this.inputRefEmail.current.style.background='yellow'
-    console.log(this.inputRefPass.current.value='123456789')
-    this.inputRefPass.current.style.color='white'
-    this.inputRefPass.current.style.background='green'
-    this.buttonRef.current.style.color='white'
-    this.buttonRef.current.style.background='green'
-  }
-  render() {
-    return (
-      <div>
-        <h1>User</h1>
-        <input type='text' placeholder='Name' ref={this.inputRefName}/><br/><br/>
-        <input type='text' placeholder='email' ref={this.inputRefEmail}/><br/><br/>
-        <input type='password' placeholder='Password' ref={this.inputRefPass}/><br/><br/>
-        <button onClick={()=>this.getRef()} ref={this.buttonRef}>Click</button>
 
-      </div>
-    )
-  }
+export default function User() {
+const [name,setName] = useState();
+const [email,setEmail] = useState();
+const [password, setPassword] = useState();
+let nameRef = useRef(null);
+let emailRef = useRef();
+let passwordRef = useRef();
+
+
+function refHendle(){
+  console.log('called', nameRef.current.value='nur')
+  nameRef.current.style.color='red'
+  nameRef.current.style.background='black'
+  console.log(emailRef.current.value='nur@gmail.com')
+  console.log(passwordRef.current.value='123456')
 }
 
-export default User;
+
+  return (
+    <div>
+      <h1>User</h1>
+      <input type='text' placeholder='Name' ref={nameRef}/><br/><br/>
+      <input type='text' placeholder='email' ref={emailRef}/><br/><br/>
+      <input type='text' placeholder='Password' ref={passwordRef}/><br/><br/>
+      <button onClick={refHendle}>Click</button>
+    </div>
+  )
+}
