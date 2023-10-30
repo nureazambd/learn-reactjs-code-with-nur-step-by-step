c11fcf1bea32af70ddd492b75c2026496b2caab5
nureazambd <nur.azam.bd@gmail.com>
2023-10-30 13:46:44 +0600

    class component using ref to chang color and value

--- src/components/User.js
+++ src/components/User.js
@@ -1,33 +1,41 @@
-import React,{useState, useMemo} from 'react'
+import React, { Component, createRef } from 'react'
 
-function User() {
-  const [count,setCount] = useState(0);
-  const [data,setData] = useState(100);
-  function countHendle(){
-    console.log('countHendle')
-    setCount(count+2)
+class User extends Component {
+  constructor(){
+    super();
+    this.inputRefName = createRef();
+    this.inputRefEmail = createRef();
+    this.inputRefPass = createRef();
+    this.buttonRef = createRef();
   }
-  function dataHendle(){
-    console.log('dataHendle')
-    setData(data*5)
-  
+  componentDidMount(){
+    console.log(this.inputRefName.current.value='Salman')
   }
+  getRef(){
+    console.log(this.inputRefName.current.value='Amir')
+    this.inputRefName.current.style.color='white'
+    this.inputRefName.current.style.background='black'
+    console.log(this.inputRefEmail.current.value='amir@gmail.com')
+    this.inputRefEmail.current.style.color='black'
+    this.inputRefEmail.current.style.background='yellow'
+    console.log(this.inputRefPass.current.value='123456789')
+    this.inputRefPass.current.style.color='white'
+    this.inputRefPass.current.style.background='green'
+    this.buttonRef.current.style.color='white'
+    this.buttonRef.current.style.background='green'
+  }
+  render() {
+    return (
+      <div>
+        <h1>User</h1>
+        <input type='text' placeholder='Name' ref={this.inputRefName}/><br/><br/>
+        <input type='text' placeholder='email' ref={this.inputRefEmail}/><br/><br/>
+        <input type='password' placeholder='Password' ref={this.inputRefPass}/><br/><br/>
+        <button onClick={()=>this.getRef()} ref={this.buttonRef}>Click</button>
 
-  const viewDataMemo = useMemo(function viewData(){
-    console.log('viewData call')
-    return data
-  },[data])
-
-  return (
-    <div>
-      <h1>Count: {count}</h1>
-      <h1>Data: {viewDataMemo}</h1>
-
-      <button onClick={countHendle}>Count</button>
-      <button onClick={dataHendle}>Data</button>
-
-
-    </div>
-  )
+      </div>
+    )
+  }
 }
+
 export default User;
