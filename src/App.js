7745cfcf1c0ab3f3fcf346d161c857e964eff697
nureazambd <nur.azam.bd@gmail.com>
2023-10-31 00:09:30 +0600

    React Router v6 install

--- src/App.js
+++ src/App.js
@@ -1,20 +1,22 @@
 import React,{useState,useRef} from 'react'
 import "./App.css";
 import "./Style.css"
-import style from "./Style.module.css"
-import User from './components/User';
-import Student from './components/Student';
-import { Button,Card,Dropdown  } from 'react-bootstrap';
-import Modal from 'react-bootstrap/Modal';
-import Reusecom from './components/Reusecom';
-import Table from 'react-bootstrap/Table';
-import Child from './components/Child';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Home from './components/Home';
+import About from './components/About';
+import Contact from './components/Contact';
+
 
 function App() {
   return (
     <div className="App">
-      <h1>App</h1>
-      <User />
+      <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/about' element={<About/>}/>
+        <Route path='/contact' element={<Contact/>}/>
+      </Routes>
+      </BrowserRouter>
     </div>
   )
 }
