0ca6df8129360057735cfd346a65704ed354cb2f
nureazambd <nur.azam.bd@gmail.com>
2023-10-30 13:14:32 +0600

    useMemo Hook

--- src/components/User.js
+++ src/components/User.js
@@ -1,38 +1,33 @@
-import React, { Component, PureComponent} from 'react'
+import React,{useState, useMemo} from 'react'
 
-function dataHendle(){
-  this.setState({data:this.state.data+5})
-}
-class User extends PureComponent {
-    
-    constructor(){
-      super();
-      this.state={
-        count:0,
-        data:10
-        
-      }
-    }
-
-     dataHendle =()=>{
-      this.setState({data:this.state.data*5})
-    }
-    render() {
-      
-      console.log('render call')
-    return (
-      <div>
-        <h1>Count: {this.state.count}</h1>
-        <h1>Data: {this.state.data}</h1>
-
-        <button onClick={()=>this.setState({count:1})}>Count+</button>
-        {/* <button onClick={()=>this.setState({data:this.state.data*2})}>Data*</button> */}
-        <button onClick={this.dataHendle}>Data*</button>
-
-
-      </div>
-    )
+function User() {
+  const [count,setCount] = useState(0);
+  const [data,setData] = useState(100);
+  function countHendle(){
+    console.log('countHendle')
+    setCount(count+2)
   }
-}
+  function dataHendle(){
+    console.log('dataHendle')
+    setData(data*5)
+  
+  }
 
+  const viewDataMemo = useMemo(function viewData(){
+    console.log('viewData call')
+    return data
+  },[data])
+
+  return (
+    <div>
+      <h1>Count: {count}</h1>
+      <h1>Data: {viewDataMemo}</h1>
+
+      <button onClick={countHendle}>Count</button>
+      <button onClick={dataHendle}>Data</button>
+
+
+    </div>
+  )
+}
 export default User;
