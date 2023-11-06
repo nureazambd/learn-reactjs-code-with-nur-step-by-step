c58f4500893ce523873a06d1208da31388925523
nureazambd <nur.azam.bd@gmail.com>
2023-11-07 00:26:08 +0600

    Update Data with API - Put Method in Reactjs

--- src/components/Filter.jsx
+++ src/components/Filter.jsx
@@ -1,63 +1,101 @@
 import React, { useEffect, useState } from 'react'
 import {useSearchParams} from 'react-router-dom'
 export default function Filter() {
-    const [searchParams, setSearchParams] = useSearchParams();
-    const age = searchParams.get('age')
-    const name = searchParams.get('name')
-    const [data, setData] = useState([]);
-
-    useEffect(()=>{
-      handelDataView();
-    },[])
-
-    function handelDataView(){
-      fetch('http://localhost:3000/user').then((result)=>{
-        result.json().then((resp)=>{
-          // console.log(resp)
-          setData(resp)
-        })
+  const [data, setData] = useState([]);
+  const [userId, setUserId] = useState(null);
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState(' ');
+  const [password, setPassword] = useState('');
+  useEffect(()=>{
+    getData();
+  },[])
+  function getData(){
+    fetch('http://localhost:3000/user').then((result)=>{
+      result.json().then((resp)=>{
+        // console.log(resp)
+        setData(resp)
+        setName(resp[0].name)
+        setEmail(resp[0].email)
+        setPassword(resp[0].password)
+        setUserId(resp[0].id)
       })
-    }
-    console.log(data)
+    })
+  }
+  console.log(data)
+  function hendelUpdate(id){
+    console.log('update',data[id-1])
+  }
+    
+  function hendelUpdate(id){
+        setName(data[id-1].name)
+        setEmail(data[id-1].email)
+        setPassword(data[id-1].password)
+        setUserId(data[id-1].id)
+  }
 
-    function deleteUser(id){
-      fetch(`http://localhost:3000/user/${id}`,{
-        method:'DELETE'
-      }).then((result)=>{
-        result.json().then((resp)=>{
-          console.log(resp)
-          handelDataView();
-        })
+  function hendelDelete(id){
+    fetch(`http://localhost:3000/user/${id}`,{
+      method:'DELETE'
+    }).then((result)=>{
+      result.json().then((resp)=>{
+        console.log('resp',resp);
+        getData();
       })
-    }
+    })
+    
+  }
+
+  function updateUser(){
+    console.log('update value', name, email, password, userId)
+    let user = {name, email, password, userId}
+    fetch(`http://localhost:3000/user/${userId}`,{
+      method:'PUT',
+      headers:{
+        'Accept':'application/json',
+        'Content-Type':'application/json'
+      },
+      body:JSON.stringify(user)
+    }).then((result)=>{
+      result.json().then((resp)=>{
+        console.log(resp);
+        getData();
+      })
+    })
+  }
+
+    
 
   return (
     <div>
-        <h1>Filter</h1>
-        <h3>Age is: {age}</h3>
-        <h3>Name is: {name}</h3>
-        <input placeholder='Enter age' onChange={(e)=>{setSearchParams({age:e.target.value,name:'Salman'})}}/><br/>
-        <button onClick={()=>{setSearchParams({age:50,name:'Salman'})}}>click</button>
-
-        
-          <table border="1">
+        <h1>Filter</h1> 
+        <table border="1">
               <tr>
-                <td>id</td>
+                <td>ID</td>
                 <td>Name</td>
                 <td>Email</td>
                 <td>Password</td>
+                <td>Option</td>
               </tr>
             {data.map((item)=>
               <tr>
-                <td>{item.id}</td>
+              <td>{item.id}</td>
               <td>{item.name}</td>
               <td>{item.email}</td>
               <td>{item.password}</td>
-              <td><button onClick={()=>{deleteUser(item.id)}}>Delete</button></td>
+              <td><button onClick={()=>hendelDelete(item.id)}>Delete</button></td>
+              <td><button onClick={()=>hendelUpdate(item.id)}>Update</button></td>
             </tr>
             )}
 
           </table>
+
+          <div>
+            <h1>Update User</h1>
+            <input value={name} onChange={(e)=>setName(e.target.value)}/><br/><br/>
+            <input value={email} onChange={(e)=>setEmail(e.target.value)}/><br/><br/>
+            <input value={password} onChange={(e)=>setPassword(e.target.value)}/><br/><br/>
+            <button onClick={updateUser}>Update</button>
+          </div>
         
     </div>
   )
